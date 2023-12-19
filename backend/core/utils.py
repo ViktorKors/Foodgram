948a47d02b86246a0a31d10bd531ce9c3163a5d0
Виктор <vitya1313vitya@gmail.com>
2023-12-19 18:32:43 +0300

    add F class

--- backend/core/utils.py
+++ backend/core/utils.py
@@ -1,16 +1,46 @@
 from django.db.models import Sum
 from django.http import HttpResponse
-
+from reportlab.pdfgen.canvas import Canvas
+from reportlab.lib.pagesizes import A4
+from reportlab.pdfbase import pdfmetrics, ttfonts
 from recipes.models import RecipeIngredient, ShoppingCart
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
+from  django.db.models import F
 
 
 class DownloadViewSet(APIView):
     """Viewset for downloading a shopping list."""
 
     permission_classes = (IsAuthenticated,)
+
+    def canvas_method(self, list, title, filename):
+        start_x, start_y = 40, 730
+        response = HttpResponse(
+            status=status.HTTP_200_OK,
+            content_type='application/pdf',
+        )
+        response['Content-Disposition'] = (f'attachment; filename='
+                                           f'"{filename}"')
+        canvas = Canvas(response, pagesize=A4)
+        pdfmetrics.registerFont(
+            ttfonts.TTFont('FreeSans', 'data/fonts/FreeSans.ttf')
+        )
+        canvas.setTitle(filename)
+        canvas.setFont('FreeSans', 34)
+        canvas.drawString(start_x - 10, start_y + 40, title)
+        canvas.setFont('FreeSans', 18)
+        for number, item in enumerate(list, start=1):
+            if start_y < 100:
+                start_y = 730
+                canvas.showPage()
+                canvas.setFont('FreeSans', 18)
+            canvas.drawString(start_x, start_y, f'{number}. {item}')
+            start_y -= 30
+        canvas.showPage()
+        canvas.save()
+        return response
 
     def merge_shopping_cart(self):
         """Creates a dictionary list with grocery purchases."""
@@ -19,9 +49,9 @@ class DownloadViewSet(APIView):
         ).values("recipe")
         items = (
             RecipeIngredient.objects.filter(recipe__in=shopping_cart)
-            .values("ingredient__name", "ingredient__measurement_unit")
+            .values(F("name"), F("measurement_unit"))
             .annotate(total_amount=Sum("amount"))
-            .order_by("ingredient__name")
+            .order_by(F("name"))
         )
         return items
 

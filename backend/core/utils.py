5ce9695bbcb95bac461016a74159f709323f8813
Виктор <vitya1313vitya@gmail.com>
2023-12-19 18:48:33 +0300

    Change download

--- backend/core/utils.py
+++ backend/core/utils.py
@@ -1,13 +1,13 @@
 from django.db.models import Sum
 from django.http import HttpResponse
-from reportlab.pdfgen.canvas import Canvas
+
+from recipes.models import RecipeIngredient, ShoppingCart
 from reportlab.lib.pagesizes import A4
 from reportlab.pdfbase import pdfmetrics, ttfonts
-from recipes.models import RecipeIngredient, ShoppingCart
+from reportlab.pdfgen.canvas import Canvas
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
-from django.db.models import F
 
 
 class DownloadViewSet(APIView):
@@ -19,24 +19,25 @@ class DownloadViewSet(APIView):
         start_x, start_y = 40, 730
         response = HttpResponse(
             status=status.HTTP_200_OK,
-            content_type='application/pdf',
+            content_type="application/pdf",
         )
-        response['Content-Disposition'] = (f'attachment; filename='
-                                           f'"{filename}"')
+        response["Content-Disposition"] = (
+            f"attachment; filename=" f'"{filename}"'
+        )
         canvas = Canvas(response, pagesize=A4)
         pdfmetrics.registerFont(
-            ttfonts.TTFont('FreeSans', 'data/fonts/FreeSans.ttf')
+            ttfonts.TTFont("FreeSans", "data/fonts/FreeSans.ttf")
         )
         canvas.setTitle(filename)
-        canvas.setFont('FreeSans', 34)
+        canvas.setFont("FreeSans", 34)
         canvas.drawString(start_x - 10, start_y + 40, title)
-        canvas.setFont('FreeSans', 18)
+        canvas.setFont("FreeSans", 18)
         for number, item in enumerate(list, start=1):
             if start_y < 100:
                 start_y = 730
                 canvas.showPage()
-                canvas.setFont('FreeSans', 18)
-            canvas.drawString(start_x, start_y, f'{number}. {item}')
+                canvas.setFont("FreeSans", 18)
+            canvas.drawString(start_x, start_y, f"{number}. {item}")
             start_y -= 30
         canvas.showPage()
         canvas.save()
@@ -49,27 +50,21 @@ class DownloadViewSet(APIView):
         ).values("recipe")
         items = (
             RecipeIngredient.objects.filter(recipe__in=shopping_cart)
-            .values(F("name"), F("measurement_unit"))
+            .values("ingredient__name", "ingredient__measurement_unit")
             .annotate(total_amount=Sum("amount"))
-            .order_by(F("name"))
+            .order_by("ingredient__name")
         )
         return items
 
     def get(self, request):
         """Returns a text file from a shopping list."""
         items = self.merge_shopping_cart()
-        text = ["Shopping list" + "\n" + "\n"]
-        for item in items:
-            text.append(
-                f"- {item['ingredient__name']} "
-                f"({item['ingredient__measurement_unit']}): "
-                f"{item['total_amount']}\n"
-            )
-        response = HttpResponse(
-            content_type="text/plain", status=status.HTTP_200_OK
-        )
-        response["Content-Disposition"] = (
-            "attachment; " "filename=shopping_cart.txt"
-        )
-        response.writelines(text)
-        return response
+        title = "Список покупок:"
+        filename = "Список покупок"
+        text_content = []
+        for ingr in items:
+            name = ingr.get("ingredient__name")
+            m_unit = ingr.get("ingredient__measurement_unit")
+            amount = ingr.get("ingredient_amount")
+            text_content.append(f"{name.capitalize()}: {amount} {m_unit}")
+        return self.canvas_method(text_content, title, filename)
